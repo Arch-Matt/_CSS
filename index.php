deb6f5b45e1b7591e73c46a8e2611cc82b5ed1e8
Matt Whetherly <matt@archcreative.co.uk>
2018-06-13 12:33:53 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body,html{
+			margin:0;
+			padding:0;
+			height:100%;
+			width:100%;
+			overflow:auto;
+			font-family: sans-serif;
+			font-size:12px;
+			font-weight:bold;
+			color:#000;
+			background:#E9E8ED;
+		}
+		.col-bg,
+		.col-bg2 { color:#fff; }
+	</style>
+	<link rel="stylesheet" type="text/css" href="matt-columns.css">
+</head>
+<body>
+
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+		<div class="m-xl-1 m-lg-2 m-md-3 m-sm-4"><div class="col-bg">1</div></div>
+	</div>
+</div>
+
+
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-2 m-lg-4 m-md-4 m-sm-6"><div class="col-bg">2</div></div>
+		<div class="m-xl-2 m-lg-4 m-md-4 m-sm-6"><div class="col-bg">2</div></div>
+		<div class="m-xl-2 m-lg-4 m-md-4 m-sm-6"><div class="col-bg">2</div></div>
+		<div class="m-xl-2 m-lg-4 m-md-4 m-sm-6"><div class="col-bg">2</div></div>
+		<div class="m-xl-2 m-lg-4 m-md-4 m-sm-6"><div class="col-bg">2</div></div>
+		<div class="m-xl-2 m-lg-4 m-md-4 m-sm-6"><div class="col-bg">2</div></div>
+	</div>
+</div>
+
+
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-3 m-lg-6 m-md-6 m-sm-12"><div class="col-bg">3</div></div>
+		<div class="m-xl-3 m-lg-6 m-md-6 m-sm-12"><div class="col-bg">3</div></div>
+		<div class="m-xl-3 m-lg-6 m-md-6 m-sm-12"><div class="col-bg">3</div></div>
+		<div class="m-xl-3 m-lg-6 m-md-6 m-sm-12"><div class="col-bg">3</div></div>
+	</div>
+</div>
+
+
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-4 m-lg-4 m-md-6 m-sm-12"><div class="col-bg">4</div></div>
+		<div class="m-xl-4 m-lg-4 m-md-6 m-sm-12"><div class="col-bg">4</div></div>
+		<div class="m-xl-4 m-lg-4 m-md-6 m-sm-12"><div class="col-bg">4</div></div>
+	</div>
+</div>
+
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-5 m-lg-5 m-md-6 m-sm-12"><div class="col-bg">5</div></div>
+		<div class="m-xl-2 m-lg-2 m-md-0 m-sm-0">0</div>
+		<div class="m-xl-5 m-lg-5 m-md-6 m-sm-12"><div class="col-bg">5</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-6 m-lg-6 m-md-6 m-sm-12"><div class="col-bg">6</div></div>
+		<div class="m-xl-6 m-lg-6 m-md-6 m-sm-12"><div class="col-bg">6</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-7 m-lg-7 m-md-6 m-sm-12"><div class="col-bg">7</div></div>
+		<div class="m-xl-5 m-lg-5 m-md-6 m-sm-12"><div class="col-bg">5</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-8 m-lg-8 m-md-6 m-sm-12"><div class="col-bg">8</div></div>
+		<div class="m-xl-4 m-lg-4 m-md-6 m-sm-12"><div class="col-bg">4</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-9 m-lg-9 m-md-6 m-sm-12"><div class="col-bg">9</div></div>
+		<div class="m-xl-3 m-lg-3 m-md-6 m-sm-12"><div class="col-bg">3</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-10 m-lg-10 m-md-6 m-sm-12"><div class="col-bg">10</div></div>
+		<div class="m-xl-2 m-lg-2 m-md-6 m-sm-12"><div class="col-bg">2</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-11 m-lg-11 m-md-6 m-sm-12"><div class="col-bg">11</div></div>
+		<div class="m-xl-1 m-lg-1 m-md-6 m-sm-12"><div class="col-bg">1</div></div>
+	</div>
+</div>
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-12 m-lg-12 m-md-12 m-sm-12"><div class="col-bg">12</div></div>
+	</div>
+</div>
+
+
+<div class="m-container-1200">
+	<div class="m-grid m-grid-gap-15 text-center">
+		<div class="m-xl-9 m-lg-9 m-md-8 m-sm-7">
+			<div class="col-bg">9</div>
+			<div class="m-grid m-grid-gap-15">
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+				<div class="m-xl-6 m-lg-6 m-md-12 m-sm-12"><div class="col-bg2">6</div></div>
+			</div>
+		</div>
+		<div class="m-xl-3 m-lg-3 m-md-4 m-sm-5">
+			<div class="col-bg">3</div>
+		</div>
+
+	</div>
+</div>
+
+
+</body>
+</html>
